6c8e0e634354244869e3174d25bd3e6ad063f4b7
Amin Khodabande <whfk233@gmail.com>
2022-12-18 12:20:39 +0330

    created user and report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity'
+import { Report } from './reports/reports.entity'
 
 @Module({
   imports: [TypeOrmModule.forRoot({ 
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [User, Report],
     synchronize: true
    }),
-     UsersModule,
+    UsersModule,
     ReportsModule],
   controllers: [AppController],
   providers: [AppService],
