3c9b01598f00fa937270db217d753e0b98ee2c16
Amin Khodabande <whfk233@gmail.com>
2022-12-20 13:52:08 +0330

    created some more routes and an interceptor

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { AfterInsert, AfterUpdate, AfterRemove, AfterLoad } from 'typeorm'
 
 @Entity()
 export class User {
@@ -10,4 +11,25 @@ export class User {
 
     @Column()
     password: string
+
+    @AfterInsert()
+    logInsert() {
+        console.log('User inserted with id ' + this.id)
+    }
+
+    @AfterUpdate()
+    updateLog() {
+        console.log('User updated with id ' + this.id)
+    }
+
+    @AfterRemove()
+    removeLog() {
+        console.log('User removed with id ' + this.id)
+    }
+
+    @AfterLoad()
+    loadLog() {
+        console.log('User loaded with id ' + this.id)
+    }
+
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { Controller, Post, Body, Get, Patch, Delete, Param, Query } from '@nestjs/common'
+import { NotFoundException } from '@nestjs/common'
+import { CreateUserDto } from './dtos/create-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
 import { UsersService } from './users.service'
+import { Serialize } from '../interceptors/serialize.interceptor'
+import { UserDto } from './dtos/user.dto'
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
 
@@ -10,5 +15,30 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    async findUser(@Param("id") id: string) {
+        const user = await this.usersService.findOne(parseInt(id))
+
+        if (!user) throw new NotFoundException('User not found')
+
+        return user
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id))
+    }
+    
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        console.log(body)
+        return this.usersService.update(parseInt(id), body)
     }
 }
